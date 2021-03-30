51351f5fda31e8a93370118a337ea88dd0a45244
Bungoh <bungohdev@gmail.com>
2021-03-29 21:44:23 -0400

    Added ProtocolLib

--- src/main/java/com/bungoh/escape/Escape.java
+++ src/main/java/com/bungoh/escape/Escape.java
@@ -6,12 +6,16 @@ import com.bungoh.escape.files.ConfigFile;
 import com.bungoh.escape.files.DataFile;
 import com.bungoh.escape.game.GameListener;
 import com.bungoh.escape.game.Manager;
+import com.bungoh.escape.game.PacketListener;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Escape extends JavaPlugin {
 
     private static Escape plugin;
+    private static ProtocolManager protocolManager;
 
     @Override
     public void onEnable() {
@@ -29,6 +33,11 @@ public final class Escape extends JavaPlugin {
         getCommand("escape").setExecutor(new BaseCommandManager());
         getCommand("escapeadmin").setExecutor(new AdminCommandManager());
 
+        //Register ProtocolLib
+        protocolManager = ProtocolLibrary.getProtocolManager();
+
+        new PacketListener();
+
         //Register Events
         Bukkit.getPluginManager().registerEvents(new GameListener(), this);
     }
@@ -41,5 +50,9 @@ public final class Escape extends JavaPlugin {
 
     public static Escape getPlugin() {
         return plugin;
+    }
+
+    public static ProtocolManager getProtocolManager() {
+        return protocolManager;
     }
 }
